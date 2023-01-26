333f8fe2fc55ba9beaaefd893596358120cd17d8
wass08 <wass08@gmail.com>
2023-01-27 08:54:31 +0900

    add landscape for pdf

--- API/REST/Screenshot.js
+++ API/REST/Screenshot.js
@@ -1,95 +1,127 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 var hardcodedAPIKey = require("../../appconfig.json").ApiKey;
-const tools = require('../shared.js');
+const tools = require("../shared.js");
 
-const os = require('os')
-const cpuCount = os.cpus().length
-var maxConcurrency = (cpuCount)/2;
-if (maxConcurrency < 1){
-    maxConcurrency = 1;
+const os = require("os");
+const cpuCount = os.cpus().length;
+var maxConcurrency = cpuCount / 2;
+if (maxConcurrency < 1) {
+  maxConcurrency = 1;
 }
 
 exports.handler = async (event, context, callback) => {
+  var sharedmem = context.sharedmem;
+  var beginPipeline = process.hrtime();
+  var proxy_server = process.env.PROXY_SERVER;
 
-    var sharedmem = context.sharedmem;
-    var beginPipeline = process.hrtime();
-    var proxy_server = process.env.PROXY_SERVER;
-
-    if ( hardcodedAPIKey != "" && event.queryStringParameters.apiKey != hardcodedAPIKey ){
-        callback(null, {
-            status: 400,
-            content: "Invalid API Key"
-        });
-        return;
-    }
-
-    while ( sharedmem.getInteger("nbPuppeteerProcess") >= maxConcurrency ){
-        await tools.sleep(20);
-    }
-
-    sharedmem.incInteger("nbPuppeteerProcess", 1);
-
-    var url = event.queryStringParameters.url;
-    if ( url == null || url == "" ){
-        url = "https://www.google.com";
-    }
-    else{
-        url = decodeURIComponent(url);
-    }
-
-    if ( !url.toLowerCase().startsWith("http://") && !url.toLowerCase().startsWith("https://") ){
-        url = "https://i.ytimg.com/vi/dQw4w9WgXcQ/maxresdefault.jpg"; //you have been rick rolled :)
-    }
-
-    var isFullPage = false; if ( event.queryStringParameters.isFullPage == "true" ) { isFullPage = true; }
-    var resX = 1280; if ( event.queryStringParameters.resX != null ) { resX = event.queryStringParameters.resX; }
-    var resY = 900; if ( event.queryStringParameters.resY != null ) { resY = event.queryStringParameters.resY; }
-    var outFormat = "jpg"; if ( event.queryStringParameters.outFormat != null ) { outFormat = event.queryStringParameters.outFormat; }
-    var waitTime = 100; if ( event.queryStringParameters.waitTime != null ) { waitTime = event.queryStringParameters.waitTime; }
-    var headers = {};
-
-    if (event.queryStringParameters.headers != null ) {
-      try {
-        headers = JSON.parse(event.queryStringParameters.headers);
-      } catch (e) {
-        console.error('can\'t parse headers');
-      }
-    }
-
-    //var screenshotResult = await tools.screnshotForUrl(url, isFullPage, resX, resY, outFormat);
-    var screenshotResult = null;
-
-    try{
-        screenshotResult = await tools.screnshotForUrlTab(url, headers, isFullPage, resX, resY, outFormat, waitTime, proxy_server);
-    }
-    catch(ex){
-        //do nothing
-
-    }
-
-    sharedmem.incInteger("nbPuppeteerProcess", -1);
-    sharedmem.incInteger("nbScreenshots", 1);
-
-    const nanoSeconds = process.hrtime(beginPipeline).reduce((sec, nano) => sec * 1e9 + nano);
-    var durationMS = (nanoSeconds/1000000);
-
-    if ( screenshotResult == null ){
-        screenshotResult = {
-            data: "",
-            mimeType: ""
-        }
-    }
-
+  if (
+    hardcodedAPIKey != "" &&
+    event.queryStringParameters.apiKey != hardcodedAPIKey
+  ) {
     callback(null, {
-            status: 200,
-            content: screenshotResult.data,
-            details: screenshotResult.details,
-            headers:{
-                "execTime": durationMS.toFixed(2) + "ms",
-                "nbPuppeteerProcess": sharedmem.getInteger("nbPuppeteerProcess"),
-                "totalScreenshots": sharedmem.getInteger("nbScreenshots"),
-                "Content-Type": screenshotResult.mimeType
-            }
+      status: 400,
+      content: "Invalid API Key",
     });
+    return;
+  }
 
+  while (sharedmem.getInteger("nbPuppeteerProcess") >= maxConcurrency) {
+    await tools.sleep(20);
+  }
+
+  sharedmem.incInteger("nbPuppeteerProcess", 1);
+
+  var url = event.queryStringParameters.url;
+  if (url == null || url == "") {
+    url = "https://www.google.com";
+  } else {
+    url = decodeURIComponent(url);
+  }
+
+  if (
+    !url.toLowerCase().startsWith("http://") &&
+    !url.toLowerCase().startsWith("https://")
+  ) {
+    url = "https://i.ytimg.com/vi/dQw4w9WgXcQ/maxresdefault.jpg"; //you have been rick rolled :)
+  }
+
+  var isFullPage = false;
+  if (event.queryStringParameters.isFullPage == "true") {
+    isFullPage = true;
+  }
+  var resX = 1280;
+  if (event.queryStringParameters.resX != null) {
+    resX = event.queryStringParameters.resX;
+  }
+  var resY = 900;
+  if (event.queryStringParameters.resY != null) {
+    resY = event.queryStringParameters.resY;
+  }
+  var outFormat = "jpg";
+  if (event.queryStringParameters.outFormat != null) {
+    outFormat = event.queryStringParameters.outFormat;
+  }
+  var orientation = "portrait";
+  if (event.queryStringParameters.orientation != null) {
+    orientation = event.queryStringParameters.orientation;
+  }
+  var waitTime = 100;
+  if (event.queryStringParameters.waitTime != null) {
+    waitTime = event.queryStringParameters.waitTime;
+  }
+  var headers = {};
+
+  if (event.queryStringParameters.headers != null) {
+    try {
+      headers = JSON.parse(event.queryStringParameters.headers);
+    } catch (e) {
+      console.error("can't parse headers");
+    }
+  }
+
+  //var screenshotResult = await tools.screnshotForUrl(url, isFullPage, resX, resY, outFormat);
+  var screenshotResult = null;
+
+  try {
+    screenshotResult = await tools.screnshotForUrlTab(
+      url,
+      headers,
+      isFullPage,
+      resX,
+      resY,
+      outFormat,
+      orientation,
+      waitTime,
+      proxy_server
+    );
+  } catch (ex) {
+    //do nothing
+  }
+
+  sharedmem.incInteger("nbPuppeteerProcess", -1);
+  sharedmem.incInteger("nbScreenshots", 1);
+
+  const nanoSeconds = process
+    .hrtime(beginPipeline)
+    .reduce((sec, nano) => sec * 1e9 + nano);
+  var durationMS = nanoSeconds / 1000000;
+
+  if (screenshotResult == null) {
+    screenshotResult = {
+      data: "",
+      mimeType: "",
+    };
+  }
+
+  callback(null, {
+    status: 200,
+    content: screenshotResult.data,
+    details: screenshotResult.details,
+    headers: {
+      execTime: durationMS.toFixed(2) + "ms",
+      nbPuppeteerProcess: sharedmem.getInteger("nbPuppeteerProcess"),
+      totalScreenshots: sharedmem.getInteger("nbScreenshots"),
+      "Content-Type": screenshotResult.mimeType,
+    },
+  });
 };
--- API/shared.js
+++ API/shared.js
@@ -1,265 +1,274 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
 var browser = null;
-module.exports.screnshotForUrlTab = async function (url, headers, isfullPage, resX, resY, outFormat, waitTime, proxy_server) {
-    return new Promise(async function (resolve, reject) {
+module.exports.screnshotForUrlTab = async function (
+  url,
+  headers,
+  isfullPage,
+  resX,
+  resY,
+  outFormat,
+  orientation,
+  waitTime,
+  proxy_server
+) {
+  return new Promise(async function (resolve, reject) {
+    try {
+      waitTime = parseInt(waitTime);
+      //prevent never finishing wait
+      if (waitTime == 0) {
+        waitTime = 1;
+      }
+    } catch (ex) {
+      waitTime = 100;
+    }
 
-        try{
-            waitTime = parseInt(waitTime);
-            //prevent never finishing wait
-            if ( waitTime == 0) {
-                waitTime = 1;
-            }
-        } catch(ex) {
-            waitTime = 100;
-        }
+    var timestamp = +new Date();
+    var buffer = null;
+    try {
+      var args = [
+        "--no-sandbox",
+        "--font-render-hinting=none",
+        "--force-color-profile=srgb",
+      ];
 
-        var timestamp = (+new Date());
-        var buffer = null;
-        try{
+      if (proxy_server != null) {
+        args.push(`--proxy-server=${proxy_server}`);
+        args.push(
+          '--host-resolver-rules="MAP * ~NOTFOUND , EXCLUDE proxyhost"'
+        );
+      }
+      if (browser == null) {
+        browser = await puppeteer.launch({
+          headless: true,
+          args: args,
+        });
+      }
 
-            var args = [
-                '--no-sandbox',
-                '--font-render-hinting=none',
-                '--force-color-profile=srgb'
-            ]
+      //const browser = await puppeteer.launch({args: ['--no-sandbox']});
+      const page = await browser.newPage();
+      await page.setUserAgent(
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36"
+      );
 
-            if (proxy_server != null){
-                args.push(`--proxy-server=${proxy_server}`)
-                args.push('--host-resolver-rules="MAP * ~NOTFOUND , EXCLUDE proxyhost"',)
-            }
-            if ( browser == null){
-                browser = await puppeteer.launch(
-                    {
-                        headless: true,
-                        args: args
-                    }
-                );
-            }
+      if (headers != null) {
+        page.setExtraHTTPHeaders(headers);
+      }
+      await page.goto(url);
 
-            //const browser = await puppeteer.launch({args: ['--no-sandbox']});
-            const page = await browser.newPage();
-            await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36");
+      await page.setViewport({
+        width: parseInt(resX),
+        height: parseInt(resY),
+        isMobile: false,
+        deviceScaleFactor: 1,
+      });
 
-            if (headers != null) {
-              page.setExtraHTTPHeaders(headers);
-            }
-            await page.goto(url);
+      //scroll the whole page for lazy loading
+      if (isfullPage) {
+        await autoScroll(page);
+      }
 
-            await page.setViewport({
-                width: parseInt(resX),
-                height: parseInt(resY),
-                isMobile: false,
-                deviceScaleFactor: 1,
-            });
+      //wait for the page to be fully loaded - max 1000ms wait
+      try {
+        await page.waitForNavigation({
+          waitUntil: "networkidle2",
+          timeout: waitTime,
+        });
+      } catch (ex) {}
 
-            //scroll the whole page for lazy loading
-            if (isfullPage){
-                await autoScroll(page);
-            }
+      var finalType = "jpg";
+      var finalMime = "image/jpeg";
 
-            //wait for the page to be fully loaded - max 1000ms wait
-            try{
-                await page.waitForNavigation({waitUntil: 'networkidle2', timeout: waitTime});
-            } catch(ex){
-            }
+      if (outFormat == "jpg") {
+        finalType = "jpeg";
+        finalMime = "image/jpeg";
+      } else if (outFormat == "png") {
+        finalType = "png";
+        finalMime = "image/png";
+      } else if (outFormat == "pdf") {
+        finalType = "pdf";
+        finalMime = "application/pdf";
+      }
 
-            var finalType = "jpg";
-            var finalMime = "image/jpeg";
+      var optionsPup = {
+        type: finalType,
+        encoding: "binary",
+        fullPage: isfullPage,
+      };
 
-            if ( outFormat == "jpg" ){
-                finalType = "jpeg";
-                finalMime = "image/jpeg";
-            }
-            else if ( outFormat == "png" ){
-                finalType = "png";
-                finalMime = "image/png";
-            }
-            else if ( outFormat == "pdf" ){
-                finalType = "pdf";
-                finalMime = "application/pdf";
-            }
+      if (finalType == "jpeg") {
+        optionsPup.quality = 88;
+      }
 
-            var optionsPup = {
-                type: finalType,
-                encoding: 'binary',
-                fullPage: isfullPage
-            };
+      if (finalType != "pdf") {
+        buffer = await page.screenshot(optionsPup);
+      } else {
+        buffer = await page.pdf({
+          printBackground: true,
+          scale: 1,
+          format: "A4",
+          landscape: orientation === "landscape",
+        });
+      }
 
-            if ( finalType == "jpeg" ){
-                optionsPup.quality = 88;
-            }
+      //await browser.close();
+      await page.close();
 
-            if ( finalType != "pdf" ){
-                buffer = await page.screenshot(optionsPup);
-            }
-            else{
-                buffer = await page.pdf({printBackground: true, scale: 1, format: 'A4'});
-            }
+      let mimeType = finalMime;
+      var resp = {
+        mimeType: mimeType,
+        data: buffer,
+      };
 
-            //await browser.close();
-            await page.close();
+      resolve(resp);
+    } catch (ex) {
+      console.log("Error while taking screenshot: " + ex.message);
+      console.log(ex);
 
-            let mimeType = finalMime;
-            var resp = {
-                mimeType: mimeType,
-                data: buffer
-            };
+      try {
+        //await browser.close();
+      } catch (ex) {}
 
-            resolve(resp);
-
-        }
-        catch(ex){
-            console.log("Error while taking screenshot: " + ex.message);
-            console.log(ex);
-
-            try{
-                //await browser.close();
-            }catch(ex){
-
-            }
-
-            var resp = {
-                status: "error",
-                details: ex.message,
-                mimeType: "application/json"
-            };
-            resolve(resp);
-        }
-    });
-}
+      var resp = {
+        status: "error",
+        details: ex.message,
+        mimeType: "application/json",
+      };
+      resolve(resp);
+    }
+  });
+};
 
 module.exports.CleanMemory = async function () {
-    await CleanMem();
+  await CleanMem();
+};
+
+async function CleanMem() {
+  if (browser != null) {
+    await browser.close();
+    browser = null;
+  }
 }
 
-async function CleanMem(){
-    if ( browser != null ){
-        await browser.close();
-        browser = null;
+module.exports.screnshotForUrl = async function (
+  url,
+  isfullPage,
+  resX,
+  resY,
+  outFormat,
+  waitTime
+) {
+  return new Promise(async function (resolve, reject) {
+    try {
+      waitTime = parseInt(waitTime);
+      //prevent never finishing wait
+      if (waitTime == 0) {
+        waitTime = 1;
+      }
+    } catch (ex) {
+      waitTime = 100;
     }
-}
 
-module.exports.screnshotForUrl = async function (url, isfullPage, resX, resY, outFormat, waitTime) {
-    return new Promise(async function (resolve, reject) {
+    var timestamp = +new Date();
+    var buffer = null;
+    var browser = null;
+    try {
+      browser = await puppeteer.launch({ args: ["--no-sandbox"] });
+      const page = await browser.newPage();
 
-        try{
-            waitTime = parseInt(waitTime);
-            //prevent never finishing wait
-            if ( waitTime == 0) {
-                waitTime = 1;
-            }
-        } catch(ex) {
-            waitTime = 100;
-        }
+      var errorContent = "";
+      await page.goto(url).catch((e) => (errorContent = e));
 
-        var timestamp = (+new Date());
-        var buffer = null;
-        var browser = null;
-        try {
+      //display error in the image
+      if (errorContent != "") {
+        page.setContent("<pre>" + errorContent.stack + "</pre>");
+      }
 
-            browser = await puppeteer.launch({args: ['--no-sandbox']});
-            const page = await browser.newPage();
+      await page.setViewport({
+        width: parseInt(resX),
+        height: parseInt(resY),
+        isMobile: false,
+        deviceScaleFactor: 1,
+      });
 
-            var errorContent = "";
-            await page.goto(url).catch(e => errorContent = e);
+      //scroll the whole page for lazy loading
+      await autoScroll(page);
 
-            //display error in the image
-            if ( errorContent != "" ){
-                page.setContent("<pre>" + errorContent.stack + "</pre>");
-            }
+      //wait for the page to be fully loaded - max 1000ms wait
+      try {
+        await page.waitForNavigation({
+          waitUntil: "networkidle2",
+          timeout: waitTime,
+        });
+      } catch (ex) {}
 
-            await page.setViewport({
-                width: parseInt(resX),
-                height: parseInt(resY),
-                isMobile: false,
-                deviceScaleFactor: 1,
-            });
+      var finalType = "jpg";
+      var finalMime = "image/jpeg";
 
-            //scroll the whole page for lazy loading
-            await autoScroll(page);
+      if (outFormat == "jpg") {
+        finalType = "jpeg";
+        finalMime = "image/jpeg";
+      } else if (outFormat == "png") {
+        finalType = "png";
+        finalMime = "image/png";
+      } else if (outFormat == "pdf") {
+        finalType = "pdf";
+        finalMime = "application/pdf";
+      }
 
-            //wait for the page to be fully loaded - max 1000ms wait
-            try{
-                await page.waitForNavigation({waitUntil: 'networkidle2', timeout: waitTime});
-            } catch(ex){
+      buffer = await page.screenshot({
+        type: finalType,
+        quality: 84,
+        encoding: "binary",
+        fullPage: isfullPage,
+      });
+      await browser.close();
 
-            }
+      let mimeType = finalMime;
+      var resp = {
+        mimeType: mimeType,
+        data: buffer,
+      };
 
-            var finalType = "jpg";
-            var finalMime = "image/jpeg";
+      resolve(resp);
+    } catch (ex) {
+      console.log("Error while taking screenshot: " + ex.message);
+      console.log(ex);
 
-            if ( outFormat == "jpg" ){
-                finalType = "jpeg";
-                finalMime = "image/jpeg";
-            }
-            else if ( outFormat == "png" ){
-                finalType = "png";
-                finalMime = "image/png";
-            }
-            else if ( outFormat == "pdf" ){
-                finalType = "pdf";
-                finalMime = "application/pdf";
-            }
+      try {
+        await browser.close();
+      } catch (ex) {}
 
-            buffer = await page.screenshot({
-                type: finalType,
-                quality: 84,
-                encoding: 'binary',
-                fullPage: isfullPage
-            });
-            await browser.close();
+      var resp = {
+        status: "error",
+        details: ex,
+        mimeType: "application/json",
+      };
+      reject(resp);
+    }
+  });
+};
 
-            let mimeType = finalMime;
-            var resp = {
-                mimeType: mimeType,
-                data: buffer
-            };
-
-            resolve(resp);
-
-        }
-        catch(ex){
-            console.log("Error while taking screenshot: " + ex.message);
-            console.log(ex);
-
-            try{
-                await browser.close();
-            }catch(ex){
-
-            }
-
-            var resp = {
-                status: "error",
-                details: ex,
-                mimeType: "application/json"
-            };
-            reject(resp);
-        }
-    });
-}
-
-
-async function autoScroll (page) {
+async function autoScroll(page) {
   await page.evaluate(async () => {
     await new Promise((resolve, reject) => {
-      let totalHeight = 0
-      let distance = 200
+      let totalHeight = 0;
+      let distance = 200;
       let timer = setInterval(() => {
-        let scrollHeight = document.body.scrollHeight
-        window.scrollBy(0, distance)
-        totalHeight += distance
-        if(totalHeight >= scrollHeight){
-          clearInterval(timer)
-          resolve()
+        let scrollHeight = document.body.scrollHeight;
+        window.scrollBy(0, distance);
+        totalHeight += distance;
+        if (totalHeight >= scrollHeight) {
+          clearInterval(timer);
+          resolve();
         }
-      }, 10)
-    })
-  })
+      }, 10);
+    });
+  });
 }
 
 module.exports.sleep = function (ms) {
   return new Promise((resolve) => {
     setTimeout(resolve, ms);
   });
-}
+};
