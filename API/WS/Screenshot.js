2fbf44ac067304181ed52db63b95bcfc9bb88c84
wass08 <wass08@gmail.com>
2023-01-27 09:36:30 +0900

    add param to ws call

--- API/WS/Screenshot.js
+++ API/WS/Screenshot.js
@@ -1,96 +1,102 @@
-const qs = require('querystring');
+const qs = require("querystring");
 var hardcodedAPIKey = require("../../appconfig.json").ApiKey;
-const tools = require('../shared.js');
+const tools = require("../shared.js");
 
-const os = require('os')
-const cpuCount = os.cpus().length
-var maxConcurrency = (cpuCount)/2;
-if (maxConcurrency < 1){
-    maxConcurrency = 1;
+const os = require("os");
+const cpuCount = os.cpus().length;
+var maxConcurrency = cpuCount / 2;
+if (maxConcurrency < 1) {
+  maxConcurrency = 1;
 }
 
 exports.upgrade = async (event, context, callback) => {
-    //console.log("Upgraded!");
+  //console.log("Upgraded!");
 };
 
 exports.open = async (event, context, callback) => {
-    callback(null, JSON.stringify({"status": "OK", "msg": "Connected to websocket screenshot service"}));
+  callback(
+    null,
+    JSON.stringify({
+      status: "OK",
+      msg: "Connected to websocket screenshot service",
+    })
+  );
 };
-
 
 var timeoutsList = {};
 
 exports.message = async (event, context, callback) => {
+  try {
+    //Do something with the message received from the client (echo, broadcast it, subscribe to a channel, execute some code ...)
+    var msg = event.body;
+    var obj = JSON.parse(msg);
+    msg = JSON.stringify(obj);
 
-    try{
+    if (hardcodedAPIKey != "" && obj.apiKey != hardcodedAPIKey) {
+      callback(
+        null,
+        JSON.stringify({
+          status: 400,
+          content: "Invalid API Key",
+        })
+      );
+      return;
+    }
 
-        //Do something with the message received from the client (echo, broadcast it, subscribe to a channel, execute some code ...)
-        var msg = event.body;
-        var obj = JSON.parse(msg);
-        msg = JSON.stringify(obj);
+    var resp = null;
 
+    if (obj.cmd == "screenshot") {
+      var sharedmem = context.sharedmem;
+      var beginPipeline = process.hrtime();
+      var proxy_server = process.env.PROXY_SERVER;
 
-        if ( hardcodedAPIKey != "" && obj.apiKey != hardcodedAPIKey ){
-            callback(null, JSON.stringify({
-                status: 400,
-                content: "Invalid API Key"
-            }));
-            return;
-        }
+      while (sharedmem.getInteger("nbPuppeteerProcess") >= maxConcurrency) {
+        await tools.sleep(20);
+      }
 
+      sharedmem.incInteger("nbPuppeteerProcess", 1);
 
-        var resp = null;
+      var url = obj.url;
+      if (url == null || url == "") {
+        url = "https://www.google.com";
+      } else {
+        url = decodeURIComponent(url);
+      }
 
-        if ( obj.cmd == "screenshot" ){
+      if (
+        !url.toLowerCase().startsWith("http://") &&
+        !url.toLowerCase().startsWith("https://")
+      ) {
+        url = "https://i.ytimg.com/vi/dQw4w9WgXcQ/maxresdefault.jpg"; //you have been rick rolled :)
+      }
 
-            var sharedmem = context.sharedmem;
-            var beginPipeline = process.hrtime();
-            var proxy_server = process.env.PROXY_SERVER
+      //var screenshotResult = await tools.screnshotForUrl(url, true);
+      var screenshotResult = null;
+      try {
+        screenshotResult = await tools.screnshotForUrlTab(
+          url,
+          obj.headers,
+          obj.isFullPage,
+          obj.resX,
+          obj.resY,
+          obj.outFormat,
+          obj.orientation,
+          obj.waitTime,
+          proxy_server
+        );
+      } catch (ex) {
+        //do nothing
+      }
 
-            while ( sharedmem.getInteger("nbPuppeteerProcess") >= maxConcurrency ){
-                await tools.sleep(20);
-            }
+      sharedmem.incInteger("nbPuppeteerProcess", -1);
+      sharedmem.incInteger("nbScreenshots", 1);
 
-            sharedmem.incInteger("nbPuppeteerProcess", 1);
+      const nanoSeconds = process
+        .hrtime(beginPipeline)
+        .reduce((sec, nano) => sec * 1e9 + nano);
+      var durationMS = nanoSeconds / 1000000;
 
-            var url = obj.url;
-            if ( url == null || url == "" ){
-                url = "https://www.google.com";
-            }
-            else{
-                url = decodeURIComponent(url);
-            }
-
-            if ( !url.toLowerCase().startsWith("http://") && !url.toLowerCase().startsWith("https://") ){
-                url = "https://i.ytimg.com/vi/dQw4w9WgXcQ/maxresdefault.jpg"; //you have been rick rolled :)
-            }
-
-            //var screenshotResult = await tools.screnshotForUrl(url, true);
-            var screenshotResult = null;
-            try{
-                screenshotResult = await tools.screnshotForUrlTab(
-                    url,
-                    obj.headers,
-                    obj.isFullPage,
-                    obj.resX,
-                    obj.resY,
-                    obj.outFormat,
-                    obj.waitTime,
-                    proxy_server,
-                );
-            }
-            catch(ex){
-                //do nothing
-            }
-
-
-            sharedmem.incInteger("nbPuppeteerProcess", -1);
-            sharedmem.incInteger("nbScreenshots", 1);
-
-            const nanoSeconds = process.hrtime(beginPipeline).reduce((sec, nano) => sec * 1e9 + nano);
-            var durationMS = (nanoSeconds/1000000);
-
-            /*
+      /*
             callback(null, {
                     status: 200,
                     content: screenshotResult.data,
@@ -102,72 +108,58 @@ exports.message = async (event, context, callback) => {
             });
             */
 
-            var b64Data = "";
-            try{
-                if (screenshotResult.data != null){
-                    b64Data = screenshotResult.data.toString("base64");
-                }
-            }
-            catch(ex){
-
-            }
-
-
-            if ( screenshotResult == null ){
-                resp = {
-                    "cmd": "responseScreenshot",
-                    "data": "",
-                    "isEmptyResult": true,
-                    "execTime": durationMS.toFixed(2) + "ms",
-                    "totalScreenshots": sharedmem.getInteger("nbScreenshots"),
-                    "originalTS": obj.originalTS,
-                    "outFormat": obj.outFormat,
-                    "Content-Type": "application/json"
-                };
-            }
-            else{
-                resp = {
-                    "cmd": "responseScreenshot",
-                    "data": b64Data,
-                    "details": screenshotResult.details,
-                    "isEmptyResult": false,
-                    "execTime": durationMS.toFixed(2) + "ms",
-                    "totalScreenshots": sharedmem.getInteger("nbScreenshots"),
-                    "originalTS": obj.originalTS,
-                    "outFormat": obj.outFormat,
-                    "Content-Type": screenshotResult.mimeType
-                };
-            }
-
-
+      var b64Data = "";
+      try {
+        if (screenshotResult.data != null) {
+          b64Data = screenshotResult.data.toString("base64");
         }
-        else if ( obj.cmd == "CleanMemory" ){
-            await tools.CleanMemory();
-            resp = {
-                "cmd": "responseCleanMemory",
-                "originalTS": obj.originalTS,
-                "status": "OK"
-            };
-        }
+      } catch (ex) {}
 
+      if (screenshotResult == null) {
+        resp = {
+          cmd: "responseScreenshot",
+          data: "",
+          isEmptyResult: true,
+          execTime: durationMS.toFixed(2) + "ms",
+          totalScreenshots: sharedmem.getInteger("nbScreenshots"),
+          originalTS: obj.originalTS,
+          outFormat: obj.outFormat,
+          "Content-Type": "application/json",
+        };
+      } else {
+        resp = {
+          cmd: "responseScreenshot",
+          data: b64Data,
+          details: screenshotResult.details,
+          isEmptyResult: false,
+          execTime: durationMS.toFixed(2) + "ms",
+          totalScreenshots: sharedmem.getInteger("nbScreenshots"),
+          originalTS: obj.originalTS,
+          outFormat: obj.outFormat,
+          "Content-Type": screenshotResult.mimeType,
+        };
+      }
+    } else if (obj.cmd == "CleanMemory") {
+      await tools.CleanMemory();
+      resp = {
+        cmd: "responseCleanMemory",
+        originalTS: obj.originalTS,
+        status: "OK",
+      };
     }
-    catch(ex){
-        console.log(ex);
-        console.log("Crash in Message handling");
-    }
+  } catch (ex) {
+    console.log(ex);
+    console.log("Crash in Message handling");
+  }
 
-
-
-    //return the response to the websocket client (caller)
-    if ( resp != null ){
-        callback(null, JSON.stringify(resp));
-    }
-
+  //return the response to the websocket client (caller)
+  if (resp != null) {
+    callback(null, JSON.stringify(resp));
+  }
 };
 
 exports.close = async (event, context, callback) => {
-
-    //here your response will be discarded because the websocket
-    //is already closed at clientside when we receive this event
-    callback(null, null);
+  //here your response will be discarded because the websocket
+  //is already closed at clientside when we receive this event
+  callback(null, null);
 };
